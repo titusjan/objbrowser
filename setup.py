23a5460e32c353a9f5d61687eb1792096427704d
Pepijn Kenter <titusjan@gmail.com>
2013-08-11 20:11:01 +0200

    setup.py installer script.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = 'objbrowser',
+    version = '0.9.0', 
+    author = "Pepijn Kenter", 
+    author_email = "titusjan@gmail.com", 
+    packages = ['objbrowser'],
+    requires = ['PySide (>=1.1.2)'])
