8ba67eb824ed968377bb32da82ab67c588631210
Pepijn Kenter <titusjan@gmail.com>
2013-12-09 15:44:20 +0100

    Take version from program.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
+from objbrowser.objectbrowser import PROGRAM_VERSION
 
 setup(name = 'objbrowser',
-    version = '0.9.1', 
+    version = PROGRAM_VERSION, 
     author = "Pepijn Kenter", 
     author_email = "titusjan@gmail.com", 
     packages = ['objbrowser'],
