3356dd42b192760b72a8476d2f6d7e7811760a9d
Pepijn Kenter <titusjan@gmail.com>
2014-03-21 23:34:57 +0100

    Added numpy_example.py

---
+++ examples/numpy_example.py
@@ -0,0 +1,31 @@
+""" 
+    Example that demonstrates inspecting a MatPlotLib Figure.
+"""
+from __future__ import print_function
+
+import sys, logging
+import numpy as np
+
+
+from objbrowser import browse, logging_basic_config
+
+logger = logging.getLogger(__name__)
+
+        
+def main():
+    """ Main program to test stand alone 
+    """
+    logging_basic_config('DEBUG')
+    logger.info('Started example')
+
+    #x = np.arange(24).reshape(6, 4)
+    x = np.zeros((2,),dtype=('i4,f4,a10'))
+    x[:] = [(1,2.,'Hello'),(2,3.,"World")]
+    
+    exit_code = browse(x.dtype.fields, obj_name='x.dtype.fields', show_special_methods=False)
+
+    logging.info('Done example')
+    sys.exit(exit_code)
+
+if __name__ == '__main__':
+    main()
