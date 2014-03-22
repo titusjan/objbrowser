8f90f523bd1f31d54482fabdf220a487c5c92e39
Pepijn Kenter <titusjan@gmail.com>
2014-03-22 23:08:55 +0100

    Default log level is INFO

--- objbrowser/__init__.py
+++ objbrowser/__init__.py
@@ -13,7 +13,7 @@ __version__ = PROGRAM_VERSION
 
 logger = logging.getLogger(__name__)
 
-def logging_basic_config(level):
+def logging_basic_config(level = 'INFO'):
     """ Setup basic config logging. Useful for debugging to quickly setup a useful logger"""
     fmt = '%(filename)20s:%(lineno)-4d : %(levelname)-7s: %(message)s'
     logging.basicConfig(level=level, format=fmt)
