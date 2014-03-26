6b0745b13888802ed81d392f16a5b2ba621d6065
Pepijn Kenter <titusjan@gmail.com>
2014-03-26 22:47:17 +0100

    Added example that modifies the summary column.

---
+++ examples/override_summary_example.py
@@ -0,0 +1,66 @@
+""" 
+    Example that demonstrates overriding the summary column
+"""
+from __future__ import print_function
+
+import sys, logging
+from objbrowser import browse, logging_basic_config
+from objbrowser.attribute_model import DEFAULT_ATTR_COLS, tio_summary
+
+logger = logging.getLogger(__name__)
+
+class MyPoint(object):
+    """ Example class for representing a point
+    """
+    def __init__(self, x, y):
+        """ Constructor
+        """
+        self.x = x
+        self.y = y
+                
+    def __repr__(self):
+        """ String representation
+        """
+        return "<MyPoint: x = {:d}, y = {:d}>".format(self.x, self.y)
+
+
+def my_summary(tree_item):
+    """ Returns (x, y) if the tree_item object is a point. 
+        Otherwise uses the default summary
+    """
+    tio = tree_item.obj
+    if isinstance(tio, MyPoint):
+        return "({}, {})".format(tio.x, tio.y)
+    else:
+        return tio_summary(tree_item)
+
+
+def my_browse(*args, **kwargs):
+    """ Creates and starts an ObjectBrowser with modified summary column.
+    """
+    attribute_columns = list(DEFAULT_ATTR_COLS)
+    summary_column = [col for col in attribute_columns if col.name == 'summary'][0]
+    summary_column.data_fn = my_summary
+    return browse(*args, attribute_columns = attribute_columns, **kwargs)
+    
+            
+def main():
+    """ Main program
+    """
+    logging_basic_config('DEBUG')
+    logger.info('Started example')
+
+    my_list = range(5, 9)
+    p1 = MyPoint(0.0, 0.0)
+    p2 = MyPoint(-4, 1)
+        
+    exit_code = my_browse({'my_list': my_list, 'p1': p1, 'p2': p2},  
+                          show_routine_attributes=False,
+                          show_special_attributes=False)
+
+    logging.info('Done example')
+    sys.exit(exit_code)
+
+
+if __name__ == '__main__':
+    main()
