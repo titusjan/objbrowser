9b4851c39d6ac65334a84af2531bb6ec0b0a325c
Pepijn Kenter <titusjan@gmail.com>
2014-03-29 12:55:46 +0100

    Made version 0.9.3

--- objbrowser/version.py
+++ objbrowser/version.py
@@ -5,6 +5,6 @@ import os
 DEBUGGING = False # TODO: False
 
 PROGRAM_NAME = 'pyobjbrowser'
-PROGRAM_VERSION = '0.9.2'
+PROGRAM_VERSION = '0.9.3'
 PROGRAM_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 #IMAGE_DIRECTORY = PROGRAM_DIRECTORY + '/images/'
