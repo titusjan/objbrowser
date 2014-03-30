228e41683b42ef1c5e4876cb7bee00c459cf2b5a
Pepijn Kenter <titusjan@gmail.com>
2014-03-30 12:01:25 +0200

    Added fields to setup in preparation for use on PyPI.

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from objbrowser.objectbrowser import PROGRAM_VERSION
 
 setup(name = 'objbrowser',
     version = PROGRAM_VERSION, 
+    description = 'Python object browser implemented in Qt.',
+    long_description = open('README.txt').read(),
+    url = 'http://pypi.python.org/pypi/objbrowser/',    
     author = "Pepijn Kenter", 
     author_email = "titusjan@gmail.com", 
+    license = 'LICENSE.txt',
     packages = ['objbrowser'],
-    requires = ['PySide (>=1.1.2)'])
+    install_requires = ['PySide (>=1.1.2)'])
+
