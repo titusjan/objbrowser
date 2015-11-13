024460ca4b6fcad7b609efbbe3bd322b0606e7cd
Pepijn Kenter <titusjan@gmail.com>
2015-11-13 16:13:51 +0100

    Fixed types in the comments.

--- objbrowser/qtimp/__init__.py
+++ objbrowser/qtimp/__init__.py
@@ -1,7 +1,8 @@
-""" Package for importing the Qt bindings (eiter PySide or PyQt), starting Qt application and
-    event loop.
+""" 
+Package for importing the Qt bindings (eiter PySide or PyQt), starting Qt application and
+event loop.
     
-Three things are done handled by this package.
+Three things are handled by this package.
 
 1)  Importing PyQT(4) or PySide. This is done in qt_importer.loadQt(). See its
     docstring for the details on how it decides between the two bindings.
@@ -11,9 +12,9 @@ Three things are done handled by this package.
     exists).
     
     If IPython is not running, the regular QtGui.QApplication().instance() is 
-    created or retreived.
+    created or retrieved.
 
-    If IPython is runing, the IPython.lib.guisupport.get_app_qt4 funtion is used 
+    If IPython is running, the IPython.lib.guisupport.get_app_qt4 function is used 
     as there may already be an event loop running (and it may be in a separate 
     kernel process). See:
     http://ipython.readthedocs.org/en/stable/api/generated/IPython.lib.guisupport.html
@@ -28,30 +29,31 @@ Three things are done handled by this package.
     therefore non-blocking. This makes user interaction from the command
     line possible.
     
-    The user can start an IPython event loop by calling the '%gui qt' magic 
-    command, starting IPython with the --qui=qt command line option, or setting
+    The user can start an IPython event loop by calling the '%gui qt' magic command, 
+    by starting IPython with the --qui=qt command line option, or by setting
     c.TerminalIPythonApp.gui = 'qt' in ~/.ipython/<profile>/ipython_config.py 
     
 
 Known issues:
     
 1)  Starting; ipython --gui=qt main.py
-    Since this will start a non blocking event loop before calling main, the
-    application exists as soon as it is created. Use the IPython -i option to
-    stay in IPython after the script. So run: ipython --gui=qt -i main.py
+    Since this will start a non-blocking event loop before calling main, the
+    application exits as soon as it is created. Use the IPython -i option to
+    stay in IPython after the script has finished. 
+    So run: ipython --gui=qt -i main.py
     
 2)  PyQT4 has two API versions: Python 2 uses API v1 by default, Python 3 
-    uses v2. PySide only implements the v2 API. The API version must be set 
+    uses v2 (PySide only implements the v2 API). The API version must be set 
     before PyQt4 is imported!
     
     This program is written for v2 so if v1 is already running, an error will
     occur. If you use the iptyhon --qui=qt command line option to start an 
-    event loop and make interaction from the command line possible, ipython-2
-    will start API v1 if PyQt is configured. To enforce ipython 2 to use the 
+    event loop (and make interaction from the command line possible), IPython-2
+    will start API v1 if PyQt is configured. To force IPython-2 to use the 
     v2 API, the QT_API environment variable must be set to 'pyqt'. 
     
     This works, unfortunately IPython 4.0.0 contains a bug and raises the 
-    following ImportError: No module named qt. As a work around, 
+    following ImportError: No module named qt. As a work around you can, 
         1: Ignore the ImportError
         2: Import PyQt4 (or PySide) manually. In IPython type: import PyQt4.QtCore
         3: Start the event loop with: %gui qt
